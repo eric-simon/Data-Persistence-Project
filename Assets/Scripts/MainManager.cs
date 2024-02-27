cc7b8e59bc9daa6f22d456982c9ea28c2bafafde
eric-simon <mister.eric@gmail.com>
2024-02-27 17:35:07 -0500

    Update MainManager.cs
    space bar will take player to name screen

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -61,7 +61,7 @@ public class MainManager : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                SceneManager.LoadScene(0);
             }
         }
     }
